78bcc74763c0726ce26b50f76440c553961fa540
Анна Дубовик <dubovik-a@pik-arenda.ru>
2019-12-04 15:52:51 +0300

    oops!

--- main.c
+++ main.c
@@ -1,13 +1,32 @@
+# include "fillit.h"
+
+void print_linked_list(t_list the_list)
+{
+	while (the_list->next)
+	{
+		ft_putstr(the_list->content);
+		the_list = the_list->next;
+	}
+	ft_putstr(the_list->content);
+	ft_putchar('\n');
+}
+
 int     main(int argc, char **argv)
 {
-	int     count;
-
+	int		count;
+	char	*result;
+	t_list *input_data;
 
 	fd = open(argv[1], O_RDONLY);
-	while (get_tetrominos(fd))
+	if(!(input_data = get_tetriminos(fd, argv))
+		return (-1);
+	// solve_task - solves tetromino puzzle
+	if (solve_task(input_data, result))
 	{
-
+			//solution stuff
 	}
-
+	print_linked_list(&input_data);
+	// TODO: add print list function
 	return (1);
+
 }
