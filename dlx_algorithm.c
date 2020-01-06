ec269b5f339d7208d51bc5a0a42ea46be0289090
cvsgbhn <kbh123456g@gmail.com>
2020-01-06 07:08:41 +0300

    added the general solution func. hope it will work. thanks this is not my official job commit

--- dlx_algorithm.c
+++ dlx_algorithm.c
@@ -1,7 +1,34 @@
 #include "dlx_algorithm.h"
 
 // cover
+void    cover(col_obj *column)
+{
+    x_node  *x_obj;
+    x_node  *x_side;
+    x_node  *x_row;
 
+    x_obj = column->list_header;
+    x_side = x_obj->left;
+    x_side->right = x_obj->right;
+    x_side = x_obj->right;
+    x_side->left = x_obj->left;
+
+    x_side = x_obj->down;
+    x_row = x_side->right;
+    while (x_side != x_obj)
+    {
+        while (x_row != x_side)
+        {
+            // is it legal to do??
+            x_row->up->down = x_row->down;
+            x_row->down->up = x_row->up;
+            x_row = x_row->right;
+        }
+        column->size = column->size - 1;
+        x_side = x_side->down;
+    }
+
+}
 // uncover
 
 // find column with fewest 1's: func to count sum of a column
@@ -18,18 +45,34 @@ int   search(col_obj *root, col_obj *tmp_sol)
 {
     col_obj *c_col;
     col_obj *copy;
+    x_node *travel;
+    x_node *pin;
 
     copy = &root;
-    if (root->next == root)
-    {
+    if (root->next == root) {
         modify_solution(&tmp_sol);
         return (1);
     }
     c_col = find_column(copy); // where root->size is min but != 0
+    travel = c_col->list_header;
     cover(c_col);
     // traverse down, in each step traverse right, cover every column, where object exists
-    while (c_col->)
-
-
-
+    while (travel != travel->C) {
+        travel = travel->down;
+        pin = travel;
+        while (travel->right != pin) {
+            cover(travel->C);
+            travel = travel->right;
+        }
+        search(root);
+        travel = pin;
+        pin = travel->C;
+        while (travel != pin) {
+            uncover(travel);
+            travel = travel->left;
+        }
+        uncover(pin);
+    }
+    uncover(pin);
+    return (0);
 }
